43c785f01c5f2a1af0cbd7bb276d7e5c96f1fe90
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-04-06 11:50:00 +0200

    add cart.actions.test.js

---
+++ client/src/redux/cart/cart.actions.test.js
@@ -0,0 +1,69 @@
+import CartActionTypes from './cart.types';
+import {
+  toggleCartHidden,
+  addItem,
+  removeItem,
+  clearItemFromCart,
+  clearCart,
+  updateCartInFirebase,
+  setCartFromFirebase
+} from './cart.actions';
+
+describe('toggleCartHidden action', () => {
+  it('should create the toggleHidden action', () => {
+    expect(toggleCartHidden().type).toEqual(CartActionTypes.TOGGLE_CART_HIDDEN);
+  });
+});
+
+describe('addItem action', () => {
+  it('should create the addItem action', () => {
+    const mockItem = {
+      id: 1
+    };
+    const action = addItem(mockItem);
+    expect(action.type).toEqual(CartActionTypes.ADD_ITEM);
+    expect(action.payload).toEqual(mockItem);
+  });
+});
+
+describe('removeItem action', () => {
+  it('should create the removeItem action', () => {
+    const mockItem = {
+      id:1
+    };
+    const action = removeItem(mockItem);
+    expect(action.type).toEqual(CartActionTypes.REMOVE_ITEM);
+    expect(action.payload).toEqual(mockItem);
+  });
+});
+
+describe('clearItemFromCart action', () => {
+  it('should create the clearItemFromCart action', () => {
+    const mockItem = {
+      id: 1
+    };
+    const action = clearItemFromCart(mockItem);
+    expect(action.type).toEqual(CartActionTypes.CLEAR_ITEM_FROM_CART);
+    expect(action.payload).toEqual(mockItem);
+  });
+});
+
+describe('clearCart action', () => {
+  it('should create the clearCart action', () => {
+    expect(clearCart().type).toEqual(CartActionTypes.CLEAR_CART);
+  });
+});
+
+describe('firebase', () => {
+    it('should update cart in firebase action', () => {
+      expect(updateCartInFirebase().type).toEqual(CartActionTypes.UPDATE_CART_IN_FIREBASE);
+    });
+
+    it('should set cart from firebase action', () => {
+        const mockCartItems = [];
+        const action = setCartFromFirebase(mockCartItems);
+
+        expect(action.type).toEqual(CartActionTypes.SET_CART_FROM_FIREBASE);
+        expect(action.payload).toEqual(mockCartItems);
+      });
+  });
